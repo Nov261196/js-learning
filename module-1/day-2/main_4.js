f90034191be8835e621c5df63be121899444bf00
HoangVu <gnar.nami@gmail.com>
2026-04-16 14:54:53 +0700

    Day 2: fix logic

--- module-1/day-2/main_4.js
+++ module-1/day-2/main_4.js
@@ -1,6 +1,8 @@
 let n = 10;
 let result = 1;
-for(let i = 1; i <= 10; i++){
-    result *= i;
-    console.log(result);
+
+for (let i = 1; i <= n; i++) {
+  result *= i;
 }
+
+console.log(result);
--- module-1/day-2/main_5.js
+++ module-1/day-2/main_5.js
@@ -1,20 +1,19 @@
-
 let n = 7;
 let nguyento = true;
 
-
-if(n <2){
-    nguyento = false;
+if (n < 2) {
+  nguyento = false;
 }
 
-for(let i = 2; i <= n; i++){
-    if(n % i === 0){
-        nguyento = false;
-        break;
-   }
-   if(nguyento){
-    console.log("la so nguyen to");
-   }else{
-    console.log("khong phai so nguyen to")
-   }
+for (let i = 2; i <= Math.sqrt(n); i++) {
+  if (n % i === 0) {
+    nguyento = false;
+    break;
+  }
+}
+
+if (nguyento) {
+  console.log("La so nguyen to");
+} else {
+  console.log("Khong phai so nguyen to");
 }
