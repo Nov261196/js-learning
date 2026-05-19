a2a5b9129662733104bbfa47ca2cb64e42e42bab
HoangVu <gnar.nami@gmail.com>
2026-05-19 15:55:20 +0700

    "day-4 finish"

---
+++ module-1/day-4/main_3.js
@@ -0,0 +1,10 @@
+let numbers = [3, 8, 2, 10, 5];
+let max = numbers[0];
+
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] > max){
+        max = numbers[i];
+    }
+}
+
+console.log(max)
---
+++ module-1/day-4/main_5.js
@@ -0,0 +1,11 @@
+function countEven(numbers){
+    let  count = 0;
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] % 2 === 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countEven([1, 2, 3, 4, 5, 6]));
